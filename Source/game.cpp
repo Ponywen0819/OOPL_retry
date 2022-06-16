7df4479989307323cca8cb6f4b56f027d5b356a8
XIAOYONG\roy25 <xiao901202@gmail.com>
2022-06-17 02:46:31 +0800

    改爆

--- Source/game.cpp
+++ Source/game.cpp
@@ -129,6 +129,7 @@ BOOL CGameApp::InitInstance()
 		return FALSE;
 
 	// The one and only window has been initialized, so show and update it.
+	m_pMainWnd->SetWindowText(_T("Little Fitghter 2"));
 	m_pMainWnd->ShowWindow(SW_SHOW);
 	m_pMainWnd->UpdateWindow();
 
